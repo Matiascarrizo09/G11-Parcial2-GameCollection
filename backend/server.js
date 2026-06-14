4aab0fe3c0c0b048ea6a666b88763a947906fbcf
matias carrizo <carrizomatiasseba@gmail.com>
2026-06-14 15:59:58 -0300

    configuracion Express y conexion PostgreSQL

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,30 @@
 require('dotenv').config();
 
+const app = require('./src/app');
 const sequelize = require('./src/config/database');
+
+const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
+    console.log("1");
+
     await sequelize.authenticate();
+
+    console.log("2");
+
     console.log('Base de datos conectada correctamente');
+
+    console.log("3");
+
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en puerto ${PORT}`);
+    });
+
+    console.log("4");
+
   } catch (error) {
-    console.error('Error al conectar la base de datos:', error);
+    console.error('Error al iniciar:', error);
   }
 }
 

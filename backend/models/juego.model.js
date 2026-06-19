322527adea3104e03094d8dfa4b083f4a86c6e23
Matko <matkoscab752@gmail.com>
2026-06-19 17:00:41 -0300

    modelos sequelize para juego y usuario

---
+++ backend/models/juego.model.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../src/config/database");
+
+const Juego = sequelize.define("Juego", {
+  id: {
+    type: DataTypes.INTEGER, // número entero
+    primaryKey: true, // establecer como primary key
+    autoIncrement: true, // aumenta automaticamente, evitando ids iguales
+  },
+  nombreJuego: {
+    type: DataTypes.STRING(50), // Equivalente a string VARCHAR(50)
+    allowNull: false,
+  },
+  desarrolladora: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+  },
+  fechaLanzamiento: {
+    type: DataTypes.DATE, // Fecha
+    allowNull: false,
+  },
+  genero: {
+    type: DataTypes.STRING(25),
+    allowNull: false,
+  },
+  horasJugadas: {
+    type: DataTypes.DECIMAL(4, 1), // Número decimal: 4 dígitos y 1 decimal
+    allowNull: false,
+    defaultValue: 0.0, // Valor por defecto
+    min: 0.0, // Validación para no permitir valores negativos
+  },
+  ultimaSesion: {
+    type: DataTypes.DATE, // Fecha
+    allowNull: true,
+  },
+  favorito: {
+    type: DataTypes.BOOLEAN, // Booleano
+    allowNull: false,
+    defaultValue: false,
+  },
+});
---
+++ backend/models/usuario.model.js
@@ -0,0 +1,41 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../src/config/database");
+
+const Usuario = sequelize.define("Usuario", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nombreUsuario: {
+    type: DataTypes.STRING(14),
+    allowNull: false,
+    unique: true,
+  },
+  contraseña: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  email: {
+    type: DataTypes.STRING(50),
+    allowNull: false,
+    unique: true,
+  },
+  mensaje: {
+    type: DataTypes.STRING(255),
+  },
+  fechaRegistro: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: Sequelize.NOW,
+  },
+  horasTotalesJugadas: {
+    type: DataTypes.DECIMAL(6, 1),
+    allowNull: false,
+    defaultValue: 0.0,
+    min: 0.0,
+  },
+});
+
+module.exports = Usuario;

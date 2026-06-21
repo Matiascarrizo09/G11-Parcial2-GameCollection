8a39da8516e7c9226b690db6677e78e753592875
matias carrizo <carrizomatiasseba@gmail.com>
2026-06-21 17:27:41 -0300

    fix: corregidas las relaciones entre entidades

--- backend/models/relaciones.js
+++ backend/models/relaciones.js
@@ -1,6 +1,6 @@
 const Juegos = require("./juego.model.js");
 const Usuarios = require("./usuario.model.js");
-const Colecciones = require("./usuario.model.js");
+const Colecciones = require("./coleccion.model.js");
 
 // Usuarios 1------N Juegos
 Usuarios.hasMany(Juegos, {
@@ -9,7 +9,7 @@ Usuarios.hasMany(Juegos, {
 
 // Juegos N--------1 Usuarios
 Juegos.belongsTo(Usuarios, {
-  foreignKey: "idJuego",
+  foreignKey: "idUsuario",
 });
 
 // Usuarios 1------N Colecciones
@@ -19,7 +19,7 @@ Usuarios.hasMany(Colecciones, {
 
 // Colecciones N---1 Usuarios
 Colecciones.belongsTo(Usuarios, {
-  foreignKey: "idColeccion",
+  foreignKey: "idUsuario",
 });
 
 // Colecciones N---M juegos
